625cfd067ee194cfada174351d91c2db7f992563
Alexander Shvets <33634796+shvets-semandex@users.noreply.github.com>
2023-10-10 20:47:26 -0400

    Initial commit

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "media-player", path: "../media-player"),
-        .package(name: "swiper", path: "../swiper")
+      .package(url: "https://github.com/shvets/media-player", from: "1.0.0"),
+      .package(url: "https://github.com/shvets/Swiper", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,7 +30,7 @@ let package = Package(
             name: "video-player",
             dependencies: [
                 "media-player",
-                "swiper"
+              .product(name: "swiper", package: "Swiper")
             ]),
         .testTarget(
             name: "video-playerTests",
--- README.md
+++ README.md
@@ -5,6 +5,6 @@ A description of this package.
 # Publishing
 
 ```bash
-git tag 1.0.0
+git tag 1.0.1
 git push --tags
 ```
