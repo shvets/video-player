804902c4b6bb28550fa1a6684be3a7f3ae72d070
Alexander Shvets <33634796+shvets-semandex@users.noreply.github.com>
2024-06-22 12:13:29 -0400

    clean up code

--- Package.swift
+++ Package.swift
@@ -7,9 +7,9 @@ let package = Package(
     name: "video-player",
     defaultLocalization: "en",
     platforms: [
-        .macOS(.v13),
-        .iOS(.v16),
-        .tvOS(.v16)
+        .macOS(.v14),
+        .iOS(.v17),
+        .tvOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-      .package(url: "https://github.com/shvets/media-player", from: "1.2.2")
+      .package(url: "https://github.com/shvets/media-player", from: "2.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
