f189efa8f60d1dff2bc002f7591286e1595399a9
Alexander Shvets <alexander.shvets@gmail.com>
2025-09-01 13:47:42 -0400

    bump version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-      .package(url: "https://github.com/shvets/media-player", from: "2.0.3"),
+      .package(url: "https://github.com/shvets/media-player", from: "2.1.0"),
       .package(url: "https://github.com/shvets/swift-ui-commons", from: "1.0.1")
     ],
     targets: [
--- README.md
+++ README.md
@@ -5,6 +5,6 @@ A description of this package.
 # Publishing
 
 ```bash
-git tag 1.4.7
+git tag 1.5.0
 git push --tags
 ```
